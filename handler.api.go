4fbb8a1aecaa06a187a60ce66de27c1de06e8050
Vladyslav <epos2006@ukr.net>
2017-09-28 21:15:36 +0300

    Update handler.api.go

--- handler.api.go
+++ handler.api.go
@@ -4,8 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"strconv"
 	"net/http"
-	"log"
-
+	"reflect"
 )
 
 
@@ -14,13 +13,21 @@ func getViewJson(c *gin.Context) {
 	if err == nil {
 		// Check if the article exists
 		view, err := getViewById(viewID)
+		arr := make(map[string]string)
+		for k,v := range view.Fields {
+			s := reflect.ValueOf(v)
+			switch s.Interface().(type){
+			case int64:
+				arr[k] = string(v.(int64))
+			default:
+				arr[k] = string(v.([]uint8))
+			}
+		}
 		if err == nil {
 			// Call the render function with the title, article and the name of the
 			// template
-			c.SecureJSON(http.StatusOK, gin.H{
-				"pl": view.Fields})
-			log.Print(view.Fields)
-
+			c.JSON(http.StatusOK, gin.H{
+				"pl": arr})
 		} else {
 			// If the article is not found, abort with an error
 			c.AbortWithError(http.StatusNotFound, err)
