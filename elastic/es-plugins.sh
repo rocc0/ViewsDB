c8f36d40ff543bd2bb53595647c0a6a69a0f69de
Vlad <epos2006@ukr.net>
2018-04-02 22:50:49 +0300

    app code structure changed

--- elastic/es-plugins.sh
+++ elastic/es-plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bin/plugin list | grep 'ukrainian' &> /dev/null
+sh bin/plugin list | grep 'ukrainian' &> /dev/null
 if [ $? == 0 ]; then
-   bin/elasticsearch-plugin install analysis-ukrainian
+   sh bin/elasticsearch-plugin install analysis-ukrainian
 fi
